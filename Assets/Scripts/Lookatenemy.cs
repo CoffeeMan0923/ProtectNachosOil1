539149ecbc58239d4ab962971d3fcaec989d1eb0
AraSilva <coffeeman0923@gmail.com>
2023-12-01 16:20:21 -0300

    Saving before error

--- Assets/Scripts/Lookatenemy.cs
+++ Assets/Scripts/Lookatenemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Lookatenemy : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     [SerializeField] float range = 15f;
     [SerializeField] Transform weapon;
     [SerializeField] GameObject particale;
@@ -19,6 +20,10 @@ public class Lookatenemy : MonoBehaviour
     void Start()
     {
         soundManager = FindObjectOfType<SoundManager>();
+        if (isoilboy)
+        {
+            animator.Play("idle");
+        }
     }
 
     void Update()
